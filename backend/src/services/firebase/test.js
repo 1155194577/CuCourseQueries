8ced236fbcecda2a89b192081c8f93a0244cbab8
1155194577 <1155194577@link.cuhk.edu.hk>
2024-09-24 22:51:00 +0800

    scraped data via scraper bot

--- backend/src/services/firebase/test.js
+++ backend/src/services/firebase/test.js
@@ -1,24 +1,24 @@
-aistCourse = {
-  code: "1000",
-  title: "Introduction to Artificial Intelligence and Machine Learning",
-  career: "Undergraduate",
-  units: "1.00",
-  grading: "Graded",
-  components: "Lecture\nProject",
-  campus: "Main Campus",
-  academic_group: "Dept of Computer Sci & Engg",
-  requirements: "",
-  description:
-    "This course covers the basic concepts, problems, approaches and applications of artificial intelligence and machine learning. It provides an introduction to various topics in AI systems and technologies, e.g., an overview of AI, machine learning theory and methods, ML in data science, neural networks and deep learning, hardware and software technologies for AI systems, natural language processing, computer vision, AI in games and sports, biomedical intelligence, intelligent manufacturing and robotics, ethical and legal issues with AI, etc. It discusses the applications of engineering principles to selected AI and ML problems. It also explores the future possibilities and challenges of AI.",
-  outcome:
-    "At the end of the course of studies, students will 1. have some basic ideas of artificial intelligence and machine learning;2. know the application areas of artificial intelligence and machine learning;3. understand the limitations and possibilities of different approaches to artificial intelligence;4. have a general picture of the overall curriculum of the AIST undergraduate programme, especially on the choice of streams.",
+const t = {
   syllabus:
     "Week 1: Introduction to the course and requirementsWeek 2: An overview of AIWeek 3: Machine learning theory and methods; data science (invited lecture by speaker from AIST/ CSE)Week 4: Neural networks and deep learning (invited lecture by speaker from AIST/ CSE)Week 5: Hardware and software technologies for AI systems (invited lecture by speaker from AIST/ CSE)Week 6: Natural language processing and applications (invited lecture by speaker from AIST/ CSE)Week 7: Computer vision; AI in games and sports (invited talk by speaker from the industry)Week 8: Biomedical intelligence (invited lecture by speaker from BME)Week 9: Intelligent manufacturing and robotics (invited lecture by speaker from MAE)Week 10-13: Project presentation and discussion",
+  career: "Undergraduate",
+  components: "Lecture\nProject",
+  requirements: "",
   required_readings:
     '1. Andrew Ng, "Machine Learning Yearning (Draft Version)," 2018, https://www.deeplearning.ai/machine-learning-yearning/ (assessed November 1, 2019).',
+  assessments: { Attendance: "20", Project: "50", Participation: "30" },
+  code: "1000",
+  grading: "Graded",
+  campus: "Main Campus",
+  description:
+    "This course covers the basic concepts, problems, approaches and applications of artificial intelligence and machine learning. It provides an introduction to various topics in AI systems and technologies, e.g., an overview of AI, machine learning theory and methods, ML in data science, neural networks and deep learning, hardware and software technologies for AI systems, natural language processing, computer vision, AI in games and sports, biomedical intelligence, intelligent manufacturing and robotics, ethical and legal issues with AI, etc. It discusses the applications of engineering principles to selected AI and ML problems. It also explores the future possibilities and challenges of AI.",
+  units: "1.00",
+  title: "Introduction to Artificial Intelligence and Machine Learning",
   recommended_readings: "",
   terms: {
-    "2024-25 Term 1": { "--LEC (8137)": [Object], "-J01-PRJ (8138)": [Object] },
+    "2024-25 Term 1": { "-J01-PRJ (8138)": [Object], "--LEC (8137)": [Object] },
   },
-  assessments: { Attendance: "20", Project: "50", Participation: "30" },
+  outcome:
+    "At the end of the course of studies, students will 1. have some basic ideas of artificial intelligence and machine learning;2. know the application areas of artificial intelligence and machine learning;3. understand the limitations and possibilities of different approaches to artificial intelligence;4. have a general picture of the overall curriculum of the AIST undergraduate programme, especially on the choice of streams.",
+  academic_group: "Dept of Computer Sci & Engg",
 };
---
+++ backend/src/webscraper/scrapData.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the cuhk-course-scraper directory
+cd ./cuhk-course-scraper
+
+# Run the main.py script with Python
+python3 main.py
