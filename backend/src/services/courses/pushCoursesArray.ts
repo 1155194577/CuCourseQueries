016af1be7220b832acf659a6b57b39ee5e204b09
1155194577 <1155194577@link.cuhk.edu.hk>
2024-09-26 09:33:05 +0800

    pushed coursesData to firestore

--- backend/src/services/courses/pushCoursesArray.ts
+++ backend/src/services/courses/pushCoursesArray.ts
@@ -1,11 +1,21 @@
 import { parseCourseData } from "./parseAndCleanData";
 import { paths } from "../../constant/paths";
 import { programmeNames } from "../../constant/programme";
-
+import { addDoc } from "../firebase/documentOperation";
+import { dbName } from "../../constant/db";
+import { CoursesArrayType } from "../../types/courses";
 for (const programmeName of programmeNames) {
   parseCourseData(`${paths.coursesFolder}/${programmeName}.json`).then(
-    (data) => {
-      console.log(data.length);
+    (CourseArray: CoursesArrayType) => {
+      for (const course of CourseArray) {
+        const courseCode: string = course.code;
+        addDoc(dbName.courseData, programmeName, courseCode, course).then(
+          (res: boolean) => {
+            console.log("success", res);
+          }
+        );
+      }
+      //   addDoc(dbName.courseData,programmeName,).then();
     }
   );
 }
